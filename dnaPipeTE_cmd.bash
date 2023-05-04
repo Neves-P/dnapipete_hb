0d18cdca5fa573e084c0b913186dca06289ff757
Neves-P <p.m.santos.neves@rug.nl>
2023-05-05 00:38:38 +0200

    add non array cmd.sh file

---
+++ dnaPipeTE_cmd.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/dnaPipeTE
+
+python3 dnaPipeTE.py -input /mnt/reads_input.fastq -output /mnt/output -RM_lib ../RepeatMasker/Libraries/RepeatMasker.lib -genome_size 170000000 -genome_coverage 0.1 -sample_number 2 -RM_t 0.2 -cpu 2 
