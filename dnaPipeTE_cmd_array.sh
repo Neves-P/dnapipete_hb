294e78544faacc92c00f1727ac57618732aa55d9
Pedro Santos Neves <10762799+Neves-P@users.noreply.github.com>
2023-05-11 15:18:15 +0200

    Add files via upload

--- dnaPipeTE_cmd_array.sh
+++ dnaPipeTE_cmd_array.sh
@@ -2,8 +2,11 @@
 
 array_number=${1} # Use this as a way to read the correct file
 
+input_path=$(sed "${array_number}q;d")
+
 cd /opt/dnaPipeTE
 
-python3 dnaPipeTE.py -input /mnt/reads_input.fastq -output /mnt/output -RM_lib ../RepeatMasker/Libraries/RepeatMasker.lib -genome_size 170000000 -genome_coverage 0.1 -sample_number 2 -RM_t 0.2 -cpu 2 
+
+python3 dnaPipeTE.py -input ${input_path} -output /mnt/output -RM_lib ../RepeatMasker/Libraries/RepeatMasker.lib -genome_size 170000000 -genome_coverage 0.1 -sample_number 2 -RM_t 0.2 -cpu 2 
 
 
